00a224de0438af6df5825497b4067fd5b8ed7d74
Stéphane Demonchaux <demonchaux.stephane@gmail.com>
2021-11-29 12:25:52 +0100

    first commit

---
+++ src/PsrLogEventListener.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JobRunner\JobRunner\PsrLog;
+
+use JobRunner\JobRunner\Event\JobEvent;
+use JobRunner\JobRunner\Event\JobFailEvent;
+use JobRunner\JobRunner\Event\JobIsLockedEvent;
+use JobRunner\JobRunner\Event\JobNotDueEvent;
+use JobRunner\JobRunner\Event\JobStartEvent;
+use JobRunner\JobRunner\Event\JobSuccessEvent;
+use JobRunner\JobRunner\Job\Job;
+use Psr\Log\LoggerInterface;
+
+use function sprintf;
+
+final class PsrLogEventListener implements JobEvent, JobFailEvent, JobSuccessEvent, JobStartEvent, JobNotDueEvent, JobIsLockedEvent
+{
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function fail(Job $job, string $output): void
+    {
+        $this->logger->error(sprintf('job %s fail : %s', $job->getName(), $output));
+    }
+
+    public function success(Job $job, string $output): void
+    {
+        $this->logger->info(sprintf('job %s success : %s', $job->getName(), $output));
+    }
+
+    public function start(Job $job): void
+    {
+        $this->logger->debug(sprintf('job %s start', $job->getName()));
+    }
+
+    public function notDue(Job $job): void
+    {
+        $this->logger->debug(sprintf('job %s notDue', $job->getName()));
+    }
+
+    public function isLocked(Job $job): void
+    {
+        $this->logger->debug(sprintf('job %s isLocked', $job->getName()));
+    }
+}
---
+++ tests/Unit/PsrLogEventListenerTest.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JobRunner\JobRunner\PsrLog\Tests\Unit\EventListener;
+
+use JobRunner\JobRunner\Job\Job;
+use JobRunner\JobRunner\PsrLog\PsrLogEventListener;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+
+/**
+ * @covers \JobRunner\JobRunner\PsrLog\PsrLogEventListener
+ */
+class PsrLogEventListenerTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $logger = self::createMock(LoggerInterface::class);
+        $job    = self::createMock(Job::class);
+
+        $job->expects($this->once())->method('getName')->willReturn('myName');
+        $logger->expects($this->once())->method('info')->with('job myName success : toto');
+
+        $sUT = new PsrLogEventListener($logger);
+
+        $sUT->success($job, 'toto');
+    }
+
+    public function testFail(): void
+    {
+        $logger = self::createMock(LoggerInterface::class);
+        $job    = self::createMock(Job::class);
+
+        $job->expects($this->once())->method('getName')->willReturn('myName');
+        $logger->expects($this->once())->method('error')->with('job myName fail : toto');
+
+        $sUT = new PsrLogEventListener($logger);
+
+        $sUT->fail($job, 'toto');
+    }
+
+    public function testNotDue(): void
+    {
+        $logger = self::createMock(LoggerInterface::class);
+        $job    = self::createMock(Job::class);
+
+        $job->expects($this->once())->method('getName')->willReturn('myName');
+        $logger->expects($this->once())->method('debug')->with('job myName notDue');
+
+        $sUT = new PsrLogEventListener($logger);
+
+        $sUT->notDue($job);
+    }
+
+    public function testIsLocked(): void
+    {
+        $logger = self::createMock(LoggerInterface::class);
+        $job    = self::createMock(Job::class);
+
+        $job->expects($this->once())->method('getName')->willReturn('myName');
+        $logger->expects($this->once())->method('debug')->with('job myName isLocked');
+
+        $sUT = new PsrLogEventListener($logger);
+
+        $sUT->isLocked($job);
+    }
+
+    public function testStart(): void
+    {
+        $logger = self::createMock(LoggerInterface::class);
+        $job    = self::createMock(Job::class);
+
+        $job->expects($this->once())->method('getName')->willReturn('myName');
+        $logger->expects($this->once())->method('debug')->with('job myName start');
+
+        $sUT = new PsrLogEventListener($logger);
+
+        $sUT->start($job);
+    }
+}
